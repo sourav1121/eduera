0ef5c5c2a42fc40e601b26aad059138aa810edfe
sourav1121 <asif2k1998@gmail.com>
2024-02-17 00:51:18 +0600

    first commit

--- index.js
+++ index.js
@@ -2,17 +2,61 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
+app.use(express.json());
+
+//Database Connection
+const uri = 'mongodb+srv://asif2k1998:TH5GbtTFjlcRPZxg@cluster0.dbcz75x.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let db;
 
 const data = require("./data/fakeData.json");
+
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+    db = client.db('myFirstDatabase');
+  })
+  .catch(err => console.error('Failed to connect to MongoDB', err));
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.get("/courses", (req, res) => {
-  res.send(data);
+app.post("/courses", async (req, res) => {
+  try {
+    const courses = db.collection('courses');
+    const course = req.body;
+    console.log(course);
+    if (course.id === null) delete course.id;
+    if (course.courses.id === null) delete course.courses.id;
+    const result = await courses.insertOne(course);
+    res.status(201).send(result);
+  } catch (err) {
+    console.error('Failed to insert course', err);
+    res.status(500).send('Failed to insert course');
+  }
+});
+
+app.get("/courses", async (req, res) => {
+  try {
+    const courses = db.collection('courses');
+    const allCourses = await courses.find().toArray();
+    res.status(200).send(allCourses);
+  } catch (err) {
+    console.error('Failed to get courses', err);
+    res.status(500).send('Failed to get courses');
+  }
 });
 
 app.get("/courses/:categoryId", (req, res) => {
